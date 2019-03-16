f159b131c1f629c4ac60f40f9ad434c6bfd23aec
jrkerr2 <johnrkerr@hotmail.com>
2019-03-16 14:36:28 -0400

    added json read + prints

--- doDataStuff.py
+++ doDataStuff.py
@@ -1,14 +1,15 @@
-# comment1
-# comment2
-# comment3
-# comment4
-# comment5
-
 # read data in from json file; print list of locations to terminal
+
+#import json python package
+import json
 
 # main executable/entry function
 def main():
     print("do stuff")
+    with open("./data/stuff.json") as filedata:
+        print(filedata)
+        objectdata=json.load(filedata)
+        print(objectdata)
 
 if __name__=="__main__":
     main()
