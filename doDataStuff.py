6fcf1d3b3c8b0a059652f66dca0dc2039a39903a
jrkerr2 <johnrkerr@hotmail.com>
2019-03-16 14:24:59 -0400

    added print from main entry point

--- doDataStuff.py
+++ doDataStuff.py
@@ -3,3 +3,12 @@
 # comment3
 # comment4
 # comment5
+
+# read data in from json file; print list of locations to terminal
+
+# main executable/entry function
+def main():
+    print("do stuff")
+
+if __name__=="__main__":
+    main()
